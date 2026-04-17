eb5ac68abd5df97e0b1b4afca98e2039fbf32efb
yegost <caza.alexandru@yahoo.com>
2026-04-17 18:10:48 +0100

    easy array question

---
+++ TypeScript/Arrays/Easy/35. Search Insert Position/Syntax.ts
@@ -0,0 +1,19 @@
+/* O(log n) */
+
+function searchInsert(nums: number[], target: number): number {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+};

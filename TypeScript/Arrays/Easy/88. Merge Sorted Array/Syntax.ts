06cbe12247f4e8db3d9e9e4344894253960de0e3
yegost <caza.alexandru@yahoo.com>
2026-04-18 07:12:58 +0100

    easy array question

---
+++ TypeScript/Arrays/Easy/88. Merge Sorted Array/Syntax.ts
@@ -0,0 +1,18 @@
+/* Time: O(m + n), Space: O(1) */
+
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+
+    while (j >= 0) {
+        if (i >= 0 && nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+};

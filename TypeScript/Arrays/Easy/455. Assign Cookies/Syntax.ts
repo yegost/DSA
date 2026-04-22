97c46feff058cf894f6be3e3e6e803422d923d32
yegost <caza.alexandru@yahoo.com>
2026-04-22 23:53:08 +0100

    easy array question

---
+++ TypeScript/Arrays/Easy/455. Assign Cookies/Syntax.ts
@@ -0,0 +1,15 @@
+function findContentChildren(g: number[], s: number[]): number {
+    g.sort((a, b) => a - b);
+    s.sort((a, b) => a - b);
+
+    let i = 0;
+    let j = 0;
+
+    while (i < g.length && j < s.length) {
+        if (s[j] >= g[i]) {
+            i++;
+        }
+        j++;
+    }
+    return i;
+}

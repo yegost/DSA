f4a012b93a86eb3c17a9bc91e060f06b2fdd8752
yegost <caza.alexandru@yahoo.com>
2026-04-24 23:58:21 +0100

    easy array question

---
+++ TypeScript/Arrays/Easy/605. Can Place Flowers/Syntax.ts
@@ -0,0 +1,15 @@
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (n === 0) return true;
+        if (
+            flowerbed[i] === 0 && 
+            (i === 0 || flowerbed[i - 1] === 0) &&
+            (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)
+        ) {
+            flowerbed[i] = 1
+            n--
+        }
+    }
+
+    return n === 0;
+};

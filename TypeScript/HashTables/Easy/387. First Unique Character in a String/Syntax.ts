3632106ee4cac387ef0d3d8f9b336ca32e6edab7
yegost <caza.alexandru@yahoo.com>
2026-04-28 23:55:31 +0100

    easy hashtables question

--- TypeScript/HashTables/Easy/387. First Unique Character in a String/Syntax.ts
+++ TypeScript/HashTables/Easy/387. First Unique Character in a String/Syntax.ts
@@ -12,3 +12,15 @@ function firstUniqChar(s: string): number {
     }
     return -1;
 };
+
+/* Map version */
+
+function firstUniqChar1(s: string): number {
+    const map = new Map<string, number>();
+
+    for (const c of s) {
+        map.set(c, (map.get(c) || 0) + 1)
+    }
+
+    return [...s].findIndex(c => map.get(c) === 1)
+};

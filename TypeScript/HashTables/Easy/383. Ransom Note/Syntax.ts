a6d5caebf48353377247f55c11b3a86ddb9a3826
yegost <caza.alexandru@yahoo.com>
2026-04-29 23:45:09 +0100

    easy hashmaps question

--- TypeScript/HashTables/Easy/383. Ransom Note/Syntax.ts
+++ TypeScript/HashTables/Easy/383. Ransom Note/Syntax.ts
@@ -1,3 +1,5 @@
+/* Memory: O(k) */
+
 function canConstruct(ransomNote: string, magazine: string): boolean {
     const count: Record<string, number> = {};
 
@@ -8,6 +10,28 @@ function canConstruct(ransomNote: string, magazine: string): boolean {
     for (const c of ransomNote) {
         if (count[c]) {
             count[c]--;
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+/* Memory: O(1) */
+
+function canConstruct1(ransomNote: string, magazine: string): boolean {
+    const count = new Array(26).fill(0);
+
+    for (const c of magazine) {
+        count[c.charCodeAt(0) - 97]++;
+    }
+
+    for (const c of ransomNote) {
+        const index = c.charCodeAt(0) - 97;
+
+        if (count[index] > 0) {
+            count[index]--;
         } else {
             return false;
         }

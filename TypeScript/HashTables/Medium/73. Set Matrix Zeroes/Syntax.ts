2dacef49dbc98cb817372f09870c884b87edbd56
yegost <caza.alexandru@yahoo.com>
2026-05-02 22:46:17 +0100

    medium hashtables question

---
+++ TypeScript/HashTables/Medium/73. Set Matrix Zeroes/Syntax.ts
@@ -0,0 +1,24 @@
+/*
+ Do not return anything, modify matrix in-place instead.
+ */
+function setZeroes(matrix: number[][]): void {
+    const rows = new Set<number>();
+    const cols = new Set<number>();
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === 0) {
+                rows.add(i);
+                cols.add(j);
+            }
+        }
+    }
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (rows.has(i) || cols.has(j)) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+};

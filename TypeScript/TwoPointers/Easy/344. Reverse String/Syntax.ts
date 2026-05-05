7a6410c9322f71ec7943f0957e26766f4c056663
yegost <caza.alexandru@yahoo.com>
2026-05-05 23:17:22 +0100

    easy two pointers question

---
+++ TypeScript/TwoPointers/Easy/344. Reverse String/Syntax.ts
@@ -0,0 +1,10 @@
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+    for (let i = 0; i < s.length / 2; i++) {
+        const temp = s[i]
+        s[i] = s[s.length - i - 1]
+        s[s.length - i - 1] = temp
+    }
+};

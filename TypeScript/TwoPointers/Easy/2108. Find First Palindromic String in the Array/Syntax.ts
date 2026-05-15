674fe23df6e7149bf2f6fdce94ebd1f871127d90
yegost <caza.alexandru@yahoo.com>
2026-05-15 19:46:05 +0100

    easy two pointers question

---
+++ TypeScript/TwoPointers/Easy/2108. Find First Palindromic String in the Array/Syntax.ts
@@ -0,0 +1,18 @@
+function firstPalindrome(words: string[]): string {
+    for (const word of words) {
+        let left = 0
+        let right = word.length - 1
+
+        while (left <= right) {
+            if (word[left] !== word[right]) break;
+
+            if (left > right) return word
+            left++;
+            right--;
+        }
+
+        if (left > right) return word;
+    }
+
+    return ""
+};

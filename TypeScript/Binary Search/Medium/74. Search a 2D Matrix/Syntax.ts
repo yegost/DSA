9c6a4a418affea5c797488ce26fb90d2e603a720
yegost <caza.alexandru@yahoo.com>
2026-05-19 23:37:54 +0100

    medium binary search question

--- TypeScript/Binary Search/Medium/74. Search a 2D Matrix/Syntax.ts
+++ TypeScript/Binary Search/Medium/74. Search a 2D Matrix/Syntax.ts
@@ -26,3 +26,32 @@ function searchMatrix(matrix: number[][], target: number): boolean {
 
     return false;
 };
+
+/* log(m * n) */
+
+function searchMatrix1(matrix: number[][], target: number): boolean {
+    let rows = matrix.length
+    let cols = matrix[0].length
+
+    let left = 0;
+    let right = rows * cols - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        const row = Math.floor(mid / cols);
+        const col = mid % cols
+
+        const value = matrix[row][col];
+
+        if (value === target) return true;
+
+        if (value > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return false;
+};

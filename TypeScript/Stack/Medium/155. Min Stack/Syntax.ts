6b3f694f2f84a971e7eaa7d2f099f0567fbf9077
yegost <caza.alexandru@yahoo.com>
2026-05-27 23:52:11 +0100

    medium stack question

---
+++ TypeScript/Stack/Medium/155. Min Stack/Syntax.ts
@@ -0,0 +1,45 @@
+class MinStack {
+    stack: number[];
+    minStack: number[];
+
+    constructor() {
+        this.stack = []
+        this.minStack = []
+    }
+
+    push(val: number): void {
+        this.stack.push(val)
+
+        if (
+            this.minStack.length === 0 ||
+            val <= this.minStack[this.minStack.length - 1]
+        ) {
+            this.minStack.push(val);
+        }
+    }
+
+    pop(): void {
+        const removed = this.stack.pop();
+
+        if (removed === this.minStack[this.minStack.length - 1]) {
+            this.minStack.pop();
+        }
+    }
+
+    top(): number {
+        return this.stack[this.stack.length - 1]
+    }
+
+    getMin(): number {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */

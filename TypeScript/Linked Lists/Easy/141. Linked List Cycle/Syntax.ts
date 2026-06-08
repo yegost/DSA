ade9a650db0885142df3c93cf59187c2fdea0577
yegost <caza.alexandru@yahoo.com>
2026-06-08 23:30:41 +0100

    easy linked list question

--- TypeScript/Linked Lists/Easy/141. Linked List Cycle/Syntax.ts
+++ TypeScript/Linked Lists/Easy/141. Linked List Cycle/Syntax.ts
@@ -16,3 +16,19 @@ function hasCycle(head: ListNode | null): boolean {
 
     return false;
 };
+
+/* Space O(1) */
+
+function hasCycle1(head: ListNode | null): boolean {
+    let slow = head;
+    let fast = head;
+
+    while (fast !== null && fast.next !== null) {
+        slow = slow!.next;
+        fast = fast.next.next;
+
+        if (slow === fast) return true;
+    }
+
+    return false;
+};

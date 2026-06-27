603d0582ac9902b959a868af05214e426531ddad
yegost <caza.alexandru@yahoo.com>
2026-06-27 23:14:33 +0100

    easy binary tree question

---
+++ TypeScript/Binary Tree/Easy/94. Binary Tree Inorder Traversal/Syntax.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+*/
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+/* Recursive */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+    let result: number[] = []
+
+    function dfs(node: TreeNode | null) {
+        if (!node) return;
+        dfs(node.left)
+        result.push(node.val)
+        dfs(node.right)
+    }
+
+    dfs(root)
+    return result;
+};

a12de68a74c6f5d1e180418c38f862c3c22681d2
yegost <caza.alexandru@yahoo.com>
2026-06-30 23:59:00 +0100

    easy binary tree question

---
+++ TypeScript/Binary Tree/Easy/101. Symmetric Tree/Syntax.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSymmetric(root: TreeNode | null): boolean {
+    if (root === null) return true;
+
+    function isMirror(left: TreeNode | null, right: TreeNode | null): boolean {
+        if (left === null && right === null) return true;
+        if (left === null || right === null) return false;
+        if (left.val !== right.val) return false;
+
+        return (
+            isMirror(left.left, right.right) &&
+            isMirror(left.right, right.left)
+        )
+    }
+
+    return isMirror(root.left, root.right)
+};

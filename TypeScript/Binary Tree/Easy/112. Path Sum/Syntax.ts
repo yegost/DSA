c9979091f57b19a11b1ae6f00d00cac4a13aafa2
yegost <caza.alexandru@yahoo.com>
2026-07-01 23:44:23 +0100

    easy binary tree question

---
+++ TypeScript/Binary Tree/Easy/112. Path Sum/Syntax.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+    if (root === null) return false;
+
+    if (root.left === null && root.right === null) {
+        return root.val === targetSum;
+    }
+
+    const remaining = targetSum - root.val;
+
+    return (
+        hasPathSum(root.left, remaining) ||
+        hasPathSum(root.right, remaining)
+    );
+}
